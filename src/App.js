56b74775d504699ec21c0dfe21890dba211dd1cd
balogun toheeb <ope4tboy@gmail.com>
2024-03-01 07:14:42 -0500

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Slide from './components/Slide'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Slide/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
